70da0d4e907137d80f814fd4a896a95bc1c96b7c
Arda Koç <ardaskoc@gmail.com>
2023-06-28 13:01:05 +0300

    updated nav button styling.

--- src/components/Icons/Globe.jsx
+++ src/components/Icons/Globe.jsx
@@ -3,7 +3,7 @@ import Globe from '../../static/img/globe-icon.png'
 export default function GlobeIcon() {
   return (
     <button className="nav-globe-icon-wrapper">
-      <img src={Globe} width="18px;" className="nav-globe-icon" />
+      <img src={Globe} width="19px;" className="nav-globe-icon" />
     </button>
   )
 }
