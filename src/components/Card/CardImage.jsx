cef21e71e04b540765fe787afe0ba84619c205e6
Arda Koç <ardaskoc@gmail.com>
2023-06-30 13:27:13 +0300

    added fav button in card component.

--- src/components/Card/CardImage.jsx
+++ src/components/Card/CardImage.jsx
@@ -1,9 +1,11 @@
 import Image from '../../static/img/card-img.png'
+import FavButton from './FavButton'
 
 export default function CardImage() {
   return (
     <div className="card-img-wrapper">
       <img src={Image} className="card-img" />
+      <FavButton />
     </div>
   )
 }
