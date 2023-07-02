2216aa4a81be0f78d6b15bf697f28401e6115ec2
Arda Koç <ardaskoc@gmail.com>
2023-07-02 14:50:19 +0300

    exported experience datas to a file.

---
+++ src/data.js
@@ -0,0 +1,47 @@
+export default [
+  {
+    id: 1,
+    title: "Learn & Plan a Kyoto trip with local advisor Yuko",
+    description: `
+      *This is 1 hour online session that you can join from anywhere in the world!*
+
+      Need help planning your Kyoto trip?
+      I'll be your online assistant to create a perfect Kyoto trip!
+      
+      - Want to know where to go sightseeing or which area to stay in?
+      - Want to understand the transportation system in Kyoto?
+      - Want to know where to go for dinner/drinks/local cafes?
+      - Looking for activities with family/partner or a solo traveler?
+      
+      Please leave it to me, I will answer all your questions;-)
+      And I'll suggest your personalized trip plan, tailored to the length of your stay and your preferences.
+    `,
+    price: 8,
+    img: "src/static/img/card-img.png",
+    review: {
+      rating: 5.0,
+      voteCount: 2,
+    },
+    location: "Japan",
+    openSpots: 5,
+  },
+  {
+    id: 2,
+    title: "Plan the perfect Customised Japan Trip with Local Expert",
+    description: `
+    All you need to have your dream trip in Japan
+
+    *If you have a date in mind and don't see any availability, don't hesitate to reach out and let me know! Let me see what we can do :)
+    
+    If you’re planning a trip to Japan but unsure or overwhelmed with all the things to do, and places to see; or if you want to avoid wasting your time on tourist traps and truly dive into the Japanese culture, then we might be exactly what you need!
+    `,
+    price: 8,
+    img: "src/static/img/card-img1.png",
+    review: {
+      rating: 5.0,
+      voteCount: 3,
+    },
+    location: "Japan",
+    openSpots: 23,
+  },
+]
