6c98f297e73a934ad8115ac9375c2d9735f9226c
Arda Koç <ardaskoc@gmail.com>
2023-07-02 14:51:46 +0300

    reorganized component and converted hard-coded datas to imported datas.

--- src/components/Experiences/Carousel.jsx
+++ src/components/Experiences/Carousel.jsx
@@ -1,30 +1,24 @@
-import '../../static/css/Carousel.css'
-import ExperienceTitle from "./ExperienceTitle"
+import "../../static/css/Carousel.css"
+import ExperiencesTitle from "./ExperiencesTitle"
 import Card from "../Card/Card"
-import Image1 from '../../static/img/card-img.png'
-import Image2 from '../../static/img/card-img1.png'
+import data from "../../data"
 
 export default function Carousel() {
-  return (
+  const experiences = data.map(experience => 
+    <Card 
+      img={experience.img}
+      rating={experience.review.rating}
+      voteCount={experience.review.voteCount}
+      location={experience.location}
+      title={experience.title}
+      price={experience.price}
+    />
+  )
+  return (    
     <div className="carousel">
-      <ExperienceTitle />
+      <ExperiencesTitle />
       <div className="experiences">
-        <Card 
-          img={Image1}
-          rating={5.0}
-          voteCount={2}
-          location="Japan"
-          title="Learn & Plan a Kyoto trip with local advisor Yuko"
-          price={8}
-        />
-        <Card 
-          img={Image2}
-          rating={5.0}
-          voteCount={3}
-          location="Japan"
-          title="Plan the perfect Customised Japan Trip with Local Expert"
-          price={8}
-        />
+        {experiences}
       </div>
     </div>
   )
--- src/components/Experiences/ExperiencesTitle.jsx
+++ src/components/Experiences/ExperiencesTitle.jsx
@@ -1,3 +1,3 @@
-export default function ExperienceTitle() {
+export default function ExperiencesTitle() {
   return <h2>Plan a trip with help from local Hosts around the world</h2>
 }
