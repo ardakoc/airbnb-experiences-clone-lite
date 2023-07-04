c5e6a1729ebe7625349255503db513aa219f004a
Arda Koç <ardaskoc@gmail.com>
2023-07-04 16:59:38 +0300

    updated attribute names.

--- src/components/Navigation/Profile.jsx
+++ src/components/Navigation/Profile.jsx
@@ -4,7 +4,7 @@ import { faCircleUser } from "@fortawesome/free-solid-svg-icons";
 export default function Profile() {
   return (
     <div className="nav-profile">
-      <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="#515151" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><line x1="3" y1="12" x2="21" y2="12"></line><line x1="3" y1="6" x2="21" y2="6"></line><line x1="3" y1="18" x2="21" y2="18"></line></svg>
+      <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="#515151" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><line x1="3" y1="12" x2="21" y2="12"></line><line x1="3" y1="6" x2="21" y2="6"></line><line x1="3" y1="18" x2="21" y2="18"></line></svg>
       <FontAwesomeIcon icon={faCircleUser} style={{color: "#717171", height: "28px" }} />
     </div>
   )
