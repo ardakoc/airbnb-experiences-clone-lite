6d8b1ca6ab7b0d9395244448b11cb509cf547df7
Arda Koç <ardaskoc@gmail.com>
2023-07-12 16:34:08 +0300

    added new component called ProfileMenu.

---
+++ src/components/Navigation/ProfileMenu/ProfileMenu.jsx
@@ -0,0 +1,16 @@
+export default function ProfileMenu() {
+  return (
+    <>
+      <menu className="profile-menu">
+        <div className="profile-menu-top">
+          <li className="text-light-bold">Sign up</li>
+          <li>Log in</li>
+        </div>
+        <div className="profile-menu-bottom">
+          <li>Airbnb your home</li>
+          <li>Help</li>
+        </div>
+      </menu>
+    </>
+  )   
+}
